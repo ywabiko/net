70b5c20ffe0c1713c32bd51e0260de41632b3fe3
Yasuhiro Wabiko <ywabiko@users.noreply.github.com>
2021-04-15 14:43:29 -0700

    a bit more useful one

--- sample.sh
+++ sample.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-curl -o /dev/null https://raw.githubusercontent.com/ywabiko/net/master/1m.sh
+x=`date`
+y=`curl -o /dev/null https://raw.githubusercontent.com/ywabiko/net/master/10m.sh -w "%{speed_download}" -s`
+echo $x,$y >> ~/speedtest.csv
+
