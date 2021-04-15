66676d00e16a5577d6aef9717e5a7f8242ab8424
Yasuhiro Wabiko <ywabiko@users.noreply.github.com>
2021-04-15 23:26:50 +0100

    changed blob size to 5m

--- speedtest.sh
+++ speedtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 x=`date`
-y=`curl -o /dev/null https://raw.githubusercontent.com/ywabiko/net/master/10m.sh -w "%{speed_download}" -s`
+y=`curl -o /dev/null https://raw.githubusercontent.com/ywabiko/net/master/5m.sh -w "%{speed_download}" -s`
 echo $x,$y >> ~/speedtest.csv
 
